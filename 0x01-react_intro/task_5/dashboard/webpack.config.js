54a5a24fc5acc3530f788bf7e356893fe360801d
Faruk <adekoladamilola4@gmail.com>
2023-08-31 21:52:39 +0100

    fixes

--- 0x01-react_intro/task_5/dashboard/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -12,20 +12,16 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: { presets: ['@babel/preset-env', '@babel/preset-react'] },
-        },
-      },
-      {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'], // Loaders for CSS files
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource', // Loader for images
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
       },
     ],
   },
