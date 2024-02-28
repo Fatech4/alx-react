04286dcc279882261fd533e0b4da5459da28eaae
Faruk <adekoladamilola4@gmail.com>
2024-02-28 22:30:10 +0100

    fix

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -10,5 +10,5 @@ export default function printBestStudents(grades) {
         student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
     }));
 
-  bestStudents.forEach((student) => console.log(`${student.firstName} ${student.lastName}`));
+  console.log(bestStudents);
 }
